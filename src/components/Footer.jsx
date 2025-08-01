8d7814630223623c940743443f4497c4db9ea1e4
Afzal20 <afzalhossen2019@gmail.com>
2025-08-01 17:20:49 +0600

    update logo color and footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-            <h2 className="text-lg">© 2025 My Ecommerce Website</h2>
+            <h2 className="text-lg">© 2025 Ecommerce Website By Afzal Hossen</h2>
         </footer>
     )
 }
